53cd2f0d2b928be077d2a09dc999e0862fbbc414
Asus <ppaogolimlim@gmail.com>
2025-06-30 23:29:14 +0800

    ongoing_homey

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -33,45 +33,39 @@
     <!------------------------------------------------ NAVBAR_HOME ----------------------------v---------------->
     <nav class="navbar navbar-expand-lg" style="background-color:#232946;">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">TickTax</a>
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('images/ticktaxx_logO.png') }}" alt="TickTax Logo" height="40" class="d-inline-block align-text-top" />
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav ms-auto me-auto">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
+                        <a class="nav-link" href="#">Events</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
                     </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                <button
+                    type="submit"
+                    class="btn custom-login-btn">
+                    Login
+                </button>
             </div>
         </div>
     </nav>
 
     <main>
-
+        <!------------------------------------------------ Dash_HOME ----------------------------v---------------->
+        
     </main>
 
 </body>
